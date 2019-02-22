b97f8e0315ca2945e22d27f27f817cdf12ef580b
Ian <19parkI@flintridgeprep.org>
2019-02-22 13:42:22 -0800

    worked

--- ColorShapes.java
+++ ColorShapes.java
@@ -1,10 +1,11 @@
 import javax.swing.*;
-import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 public class ColorShapes extends JPanel{
     int xPosition;
     int yPosition;
+    Timer newTimer = new Timer(17, new TimerListener());
+
     @Override
     public void paintComponent(Graphics g){
         g.fillRect(0, 0, this.getWidth(), this.getHeight());
@@ -13,23 +14,43 @@ public class ColorShapes extends JPanel{
         int green = 255;
         Color gColor = new Color(red, green, blue);
         g.setColor(gColor);
-        g.fillOval(xPosition+1, yPosition+1, 100, 100);
-        
+        g.fillOval(xPosition, yPosition, 100, 100);
 
     }
 
     public static void main(){
-        
+
         JFrame f = new JFrame();
         ColorShapes i = new ColorShapes();
+        i.newTimer.start();
         f.add(i);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setSize(300, 300);
         f.setVisible(true);
-        while(true){
-            i.xPosition++;
-            i.yPosition++;
-            f.repaint();
+
+        //while(true){
+        // i.xPosition++;
+        // i.yPosition++;
+        //f.repaint();
+        //}
+    }
+    class TimerListener implements ActionListener{//Make a variable that can be either 1 or -1
+        @Override
+        public void actionPerformed(ActionEvent event){
+            xPosition++;
+            yPosition++;
+            ColorShapes.this.repaint();
+            if(xPosition==300){
+                xPosition--;
+            } else if(xPosition==0){
+                xPosition++;
+            } 
+            if(yPosition==300){
+                yPosition--;
+
+            } else if(yPosition==0){
+                yPosition++;
+            }
         }
     }
 }
--- ExampleSwingTimer.java
+++ ExampleSwingTimer.java
@@ -1,15 +1,38 @@
-
-//public class ExampleSwingTimer{
- //   public static void main(){
- //  int period = 1000;
-   //Timer myTimer = new Timer(period, new Timerlistener();
-   //myTimer.start();
-//}
-   //class TimerListener implements ActionListener{
-       //@Override
-       //public void actionPerformed(ActionEvent event){
-           
-       // }
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+public class ExampleSwingTimer {
+    JFrame f = new JFrame();
+    JLabel l = new JLabel("wassup");
+    int seconds =0;
+    
+    Timer myTimer = new Timer(1000, new TimerListener());
+    public ExampleSwingTimer(){
        
- //   }
-//}
+        
+        //int period = 1000;
+        //Timer myTimer = new Timer(period, new TimerListener());
+        myTimer.start();
+        f.add(l);
+        
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(200, 200);
+        f.setVisible(true);
+        
+    }
+    public static void main(){
+        ExampleSwingTimer ex = new ExampleSwingTimer();
+    }
+    class TimerListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent event){
+            seconds++;
+            l.setText( seconds+" seconds");
+            f.repaint();
+            if(seconds==5){
+                myTimer.stop();
+            }
+        }
+
+    }
+}
